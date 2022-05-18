97988f8be00f858f9a6f3e0135562f5dd54f2b80
DylanFennelly <20093427@mail.wit.ie>
2022-05-18 16:33:57 +0100

    XML save and load

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module ng.plan.nationalgalleryplanner {
     requires javafx.controls;
     requires javafx.fxml;
+    requires xstream;
 
 
-    opens ng.plan.nationalgalleryplanner to javafx.fxml;
+    opens ng.plan.nationalgalleryplanner to javafx.fxml, xstream;
+    opens ng.plan.nationalgalleryplanner.ADTs to javafx.fxml, xstream;
+
+
+    exports ng.plan.nationalgalleryplanner.ADTs;
     exports ng.plan.nationalgalleryplanner;
 }
--- src/main/java/ng/plan/nationalgalleryplanner/NGPRun.java
+++ src/main/java/ng/plan/nationalgalleryplanner/NGPRun.java
@@ -1,24 +1,30 @@
 package ng.plan.nationalgalleryplanner;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.security.AnyTypePermission;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.Objects;
 
 public class NGPRun extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
         Parent p = FXMLLoader.load(Objects.requireNonNull(NGPRun.class.getResource("main-view.fxml")));
         Scene scene = new Scene(p);
         stage.setTitle("National Gallery Route Planner");
         stage.setScene(scene);
         stage.show();
+
+        boolean xmlExists = new File("ngpRooms.xml").isFile();
+        if (!xmlExists)
+            save(); //creates an xml file if it doesnt exit yet
+        load();
 
     }
 
@@ -28,9 +34,18 @@ public class NGPRun extends Application {
 
     public static void save() throws Exception {
         XStream xStream = new XStream((new DomDriver()));
-        ObjectOutputStream out = xStream.createObjectOutputStream(new FileWriter("auctionApp.xml"));
-        out.writeObject(AuctionApp.DRIVER);
+        ObjectOutputStream out = xStream.createObjectOutputStream(new FileWriter("ngpRooms.xml"));
+
         out.close();
-        System.out.println("Saved to auctionApp.xml");
+        System.out.println("Saved to ngpRooms.xml");
+    }
+
+    public static void load() throws Exception {
+        XStream xStream = new XStream((new DomDriver()));
+        xStream.addPermission(AnyTypePermission.ANY);       //granting permissions to set read object to the driver  | from: https://stackoverflow.com/questions/30812293/com-thoughtworks-xstream-security-forbiddenclassexception
+        ObjectInputStream in = xStream.createObjectInputStream(new FileReader("ngpRooms.xml"));
+
+        in.close();
+        System.out.println("Loaded from ngpRooms.xml");
     }
 }

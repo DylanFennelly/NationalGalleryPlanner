366310a3a494f0f3693c7ddd794cf3c36e3cac5a
DylanFennelly <20093427@mail.wit.ie>
2022-05-18 14:58:49 +0100

    Renamed initial files

--- src/main/java/ng/plan/nationalgalleryplanner/NGPController.java
+++ src/main/java/ng/plan/nationalgalleryplanner/NGPController.java
@@ -3,7 +3,7 @@ package ng.plan.nationalgalleryplanner;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class NGPController {
     @FXML
     private Label welcomeText;
 
